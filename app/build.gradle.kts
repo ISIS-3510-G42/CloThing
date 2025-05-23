95ae7d8027e175115f0b569e603864ff47227690
mamesam1 <ma.mesam1@uniandes.edu.co>
2025-05-23 17:37:39 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,121 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    //(libs.plugins.googleService)
+    id("com.google.firebase.firebase-perf")
+    id("com.google.gms.google-services")
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+}
+
+
+android {
+    namespace = "com.moviles.clothingapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.moviles.clothingapp"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    //Retrofit and moshi for API calls and parsing
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.moshi:moshi:1.15.2")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    // Jetpack Compose BOM
+    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+
+    // Coil (Image Loading for Compose)
+    implementation("io.coil-kt:coil-compose:2.2.2")
+
+    // Added for this project compose UI, icons, nav and fonts
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation ("androidx.compose.ui:ui:1.5.0")
+    implementation ("androidx.compose.material3:material3:1.1.1")
+    implementation ("androidx.navigation:navigation-compose:2.7.4")
+    implementation ("androidx.compose.material:material-icons-extended:1.5.0")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.6.0")
+
+    // Firebase auth
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-perf")
+    implementation ("com.google.firebase:firebase-analytics:17.4.1")
+
+
+    // Camera dependencies
+    implementation ("androidx.camera:camera-core:1.3.0")
+    implementation ("androidx.camera:camera-lifecycle:1.3.0")
+    implementation ("androidx.camera:camera-view:1.3.0")
+    implementation ("androidx.compose.ui:ui:1.5.0")
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+
+    // Maps
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.maps.android:maps-compose:2.11.4")
+
+    // Image Storage
+    implementation("io.appwrite:sdk-for-android:6.1.0")
+
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.navigation.runtime.android)
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.play.services.location)
+    implementation(libs.play.services.measurement.api)
+    implementation(libs.play.services.maps)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+
---
+++ app/src/main/java/com/moviles/clothingapp/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.moviles.clothingapp
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.moviles.clothingapp.navigation.AppNavigation
+import com.moviles.clothingapp.viewmodel.LoginViewModel
+import com.moviles.clothingapp.viewmodel.ResetPasswordViewModel
+import com.moviles.clothingapp.viewmodel.WeatherViewModel
+import android.Manifest
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
+import com.google.firebase.perf.FirebasePerformance
+
+
+class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var resetPasswordViewModel: ResetPasswordViewModel
+    private lateinit var weatherViewModel: WeatherViewModel
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    private var locationPermissionGranted = false
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        locationPermissionGranted = permissions.entries.all { it.value }
+        weatherViewModel = WeatherViewModel(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+        loginViewModel = LoginViewModel(auth)
+        resetPasswordViewModel = ResetPasswordViewModel(auth)
+        Log.d("FirebasePerf", "Firebase Performance Monitoring initialized: ${FirebasePerformance.getInstance()}")
+        firebaseAnalytics = Firebase.analytics
+
+        val deviceInfo = Bundle().apply {
+            putString("device_model", Build.MODEL)
+            putString("device_brand", Build.BRAND)
+            putString("os_version", Build.VERSION.RELEASE)
+        }
+
+        firebaseAnalytics.logEvent("device_info", deviceInfo)
+        Log.d("DEVICES", ""+deviceInfo)
+
+        // Request location permissions
+        requestLocationPermissions()
+
+        setContent {
+            val navController = rememberNavController()
+            AppNavigation(navController, loginViewModel, resetPasswordViewModel, weatherViewModel)
+        }
+    }
+
+    private fun requestLocationPermissions() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // Permissions already granted
+                locationPermissionGranted = true
+                weatherViewModel = WeatherViewModel(this)
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
+            }
+        }
+    }
+}
